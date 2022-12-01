bb361c3b4b28255cde65411ff45f4e795764d317
Quinlan <3247596711@qq.com>
2022-12-01 10:52:31 +0800

    init project

---
+++ src/test/java/edu/whu/learneur/LearneurApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.whu.learneur;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearneurApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
