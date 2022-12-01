4b3764cfbfce34295bac0cbdf6879c8a5d3e8ee8
Akihabara <2607038766@qq.com>
2022-12-01 13:45:09 +0800

    lxy_dev: 初始化操作

---
+++ src/main/java/edu/whu/learneur/LearneurApplication.java
@@ -0,0 +1,13 @@
+package edu.whu.learneur;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearneurApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LearneurApplication.class, args);
+    }
+
+}
